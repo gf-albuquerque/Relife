26d7305d7b1b010770237b5f38963b27e53f5819
Gabriel F Albuquerque <gfonteles.a@gmail.com>
2022-11-25 22:56:49 -0300

    add getbyiduser route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,18 @@ AppDataSource.initialize()
 
     app.use(routes);
 
+    app.use((err: Error, request: Request, response: Response) => {
+      if (err instanceof Error) {
+        return response.status(400).json({
+          error: err.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+      });
+    });
     app.listen(3333, () => console.log("Server is running"));
   })
   .catch((err) => {
